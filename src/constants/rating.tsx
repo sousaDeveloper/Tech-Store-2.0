46227cf924a31c21e7a8fb01397d97217d4e6367
sousaDeveloper <devlpsousa@gmail.com>
2025-01-13 12:30:30 -0300

    feat: add category-icon and rating constants

---
+++ src/constants/rating.tsx
@@ -0,0 +1,11 @@
+import Rating from "@/helpers/rating";
+
+export const RATING = {
+  clientOne: <Rating rating={4.5} />,
+  clientTwo: <Rating rating={5.0} />,
+  clientThird: <Rating rating={3.5} />,
+  clientFour: <Rating rating={4.6} />,
+  clientFive: <Rating rating={3.9} />,
+  clientSix: <Rating rating={5.0} />,
+  clientSeven: <Rating rating={4.8} />,
+};

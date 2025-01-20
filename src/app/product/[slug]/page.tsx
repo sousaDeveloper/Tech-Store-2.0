cc1c20c836c512b2a883690c3a79d9551c2e5935
sousaDeveloper <devlpsousa@gmail.com>
2025-01-20 13:49:38 -0300

    feat: add filter for products in the same category

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import ProductDetails from "../components/ProductDetails";
 import { computeProductTotalPrice } from "@/helpers/product";
+import ProductList from "@/app/components/ProductList";
 
 interface ProductPageProps {
   params: {
@@ -17,16 +18,33 @@ const ProductPage = async ({ params }: ProductPageProps) => {
     },
   });
 
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        id: product?.categoryID,
+      },
+      NOT: {
+        slug: slug,
+      },
+    },
+  });
+
   if (!product) return <h1>Produto não encontrado</h1>;
 
   return (
     product && (
-      <ProductDetails
-        product={{
-          ...product,
-          totalPrice: computeProductTotalPrice(product),
-        }}
-      />
+      <>
+        <ProductDetails
+          product={{
+            ...product,
+            totalPrice: computeProductTotalPrice(product),
+          }}
+        />
+        <div className="flex flex-col pb-20 text-secondaryColor px-5">
+          <h2 className="text-lg mb-2">Produtos Relacionados</h2>
+          <ProductList products={products} />
+        </div>
+      </>
     )
   );
 };

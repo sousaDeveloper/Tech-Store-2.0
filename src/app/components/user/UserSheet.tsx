e258d5968e49f07ef821a47086eba87ae1da8df1
sousaDeveloper <devlpsousa@gmail.com>
2025-01-23 14:37:21 -0300

    feat: create SignUp and SignIn Components

--- src/app/components/user/UserSheet.tsx
+++ src/app/components/user/UserSheet.tsx
@@ -9,14 +9,44 @@ import {
 import { DialogTitle } from "@radix-ui/react-dialog";
 import { ChevronRight, User2Icon } from "lucide-react";
 import Link from "next/link";
-import CustomInput from "./CustomInput/CustomInput";
 import ButtonGoogle from "./ButtonGoogle";
+import { FormProvider, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 interface UserSheetProps {
   className?: string;
 }
 
+const formSchema = z.object({
+  email: z.string().email("Email inválido").min(1, "Email é obrigatório"),
+  password: z
+    .string()
+    .min(8, "A senha deve ter pelo menos 8 caracteres.")
+    .min(1, "Senha é obrigatória"),
+});
+
 const UserSheet = ({ className }: UserSheetProps) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
     <Sheet>
       <SheetTrigger
@@ -36,29 +66,53 @@ const UserSheet = ({ className }: UserSheetProps) => {
         </div>
 
         <DialogTitle className="text-2xl mt-10">Acesse sua conta!</DialogTitle>
+        <FormProvider {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem className="text-left">
+                  <FormLabel className="text-md">Email</FormLabel>
+                  <FormControl>
+                    <Input type="text" {...field} />
+                  </FormControl>
+                  <FormMessage className="text-red-400 text-start" />
+                </FormItem>
+              )}
+            />
 
-        <div>
-          <form className="flex flex-col gap-2">
-            <CustomInput label="Email" type="text" />
-            <CustomInput label="Senha" type="password" />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem className="text-left">
+                  <FormLabel className="text-md">Senha</FormLabel>
+                  <FormControl>
+                    <Input type="text" {...field} />
+                  </FormControl>
+                  <FormMessage className="text-red-400 text-start" />
+                </FormItem>
+              )}
+            />
 
             <button className="w-full py-2 bg-gradient mt-5 rounded-lg">
               Acessar
             </button>
+            <div className="flex justify-center items-center space-x-4 my-5">
+              <hr className="flex-grow border-t-1 border-gray-300" />
+              <h3 className="text-sm opacity-70">Ou faça login com</h3>
+              <hr className="flex-grow border-t-1 border-gray-300" />
+            </div>
+            <ButtonGoogle text="Continuar com o Google" />
+            <h3 className="text-sm text-center mt-4">
+              Não possui uma conta?{" "}
+              <Link className="underline" href="/sign-up">
+                Registre-se
+              </Link>
+            </h3>
           </form>
-          <div className="flex justify-center items-center space-x-4 my-5">
-            <hr className="flex-grow border-t-1 border-gray-300" />
-            <h3 className="text-sm opacity-70">Ou faça login com</h3>
-            <hr className="flex-grow border-t-1 border-gray-300" />
-          </div>
-          <ButtonGoogle text="Fazer login com o Google" />
-        </div>
-        <h3 className="text-sm text-center">
-          Não possui uma conta?{" "}
-          <Link className="underline" href="/sign-up">
-            Registre-se
-          </Link>
-        </h3>
+        </FormProvider>
       </SheetContent>
     </Sheet>
   );
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -2,43 +2,148 @@
 
 import { ChevronLeft } from "lucide-react";
 import { useRouter } from "next/navigation";
-import CustomInput from "../components/user/CustomInput/CustomInput";
 import ButtonGoogle from "../components/user/ButtonGoogle";
+import { FormProvider, useForm } from "react-hook-form";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z
+  .object({
+    username: z.string().min(2, {
+      message: "O nome de usuário deve ter pelo menos 2 caracteres.",
+    }),
+    email: z.string().email("Email inválido").min(1, "Email é obrigatório"),
+    password: z
+      .string()
+      .min(8, "A senha deve ter pelo menos 8 caracteres.")
+      .min(1, "Senha é obrigatória"),
+    confirmPassword: z
+      .string()
+      .min(8, "A senha de confirmação deve ter pelo menos 8 caracteres."),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Senhas não coincidem",
+  });
 
 const SignUp = () => {
   const router = useRouter();
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
-    <main className="flex flex-col text-secondaryColor text-center">
-      <div className="bg-blur-purple absolute top-0 left-0 w-full h-full z-[-1]"></div>
-      <section className="px-8 mt-20">
+    <main className="flex flex-col text-secondaryColor min-h-[48rem] text-center">
+      <div className="bg-blur-purple absolute top-0 left-0 w-full min-h-[48rem] z-[-1]"></div>
+      <section className="px-8 mt-12">
         <ChevronLeft
           size={36}
           onClick={() => router.back()}
           className="cursor-pointer bg-background top-6 left-5 rounded-xl absolute"
         />
-        <h2 className="text-2xl mt-10">Criar Conta</h2>
+        <h2 className="text-2xl mt-5">Criar Conta</h2>
         <h3 className="text-sm opacity-60">
           Preencha suas informações abaixo e registre-se.
         </h3>
-        <div>
-          <form className="flex flex-col gap-2">
-            <CustomInput label="Nome" type="text" />
-            <CustomInput label="Email" type="text" />
-            <CustomInput label="Senha" type="password" />
-            <CustomInput label="Confirmar senha" type="password" />
+        <FormProvider {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 mt-10"
+          >
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem className="text-left">
+                  <FormLabel className="text-md">Nome de Usuário</FormLabel>
+                  <FormControl>
+                    <Input type="text" className="mt-0" {...field} />
+                  </FormControl>
+                  <FormMessage className="text-red-400 text-start" />
+                </FormItem>
+              )}
+            />
 
-            <button className="w-full py-2 bg-gradient mt-5 rounded-lg">
+            {/* Campo de Email */}
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem className="text-left">
+                  <FormLabel className="text-md">Email</FormLabel>
+                  <FormControl>
+                    <Input type="email" {...field} />
+                  </FormControl>
+                  <FormMessage className="text-red-400 text-start" />
+                </FormItem>
+              )}
+            />
+
+            {/* Campo de Senha */}
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem className="text-left">
+                  <FormLabel className="text-md">Senha</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage className="text-red-400 text-start" />
+                </FormItem>
+              )}
+            />
+
+            {/* Campo de Confirmação de Senha */}
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem className="text-left">
+                  <FormLabel className="text-md">Confirmar Senha</FormLabel>
+                  <FormControl className="m-0">
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage className="text-red-400 text-start" />
+                </FormItem>
+              )}
+            />
+
+            <button
+              className="w-full py-2 bg-gradient mt-5 rounded-lg"
+              type="submit"
+            >
               Criar Conta
             </button>
+
             <div className="flex justify-center items-center space-x-4 mt-4">
               <hr className="flex-grow border-t-1 border-gray-300" />
               <h3 className="text-sm opacity-60">Ou crie sua conta com</h3>
               <hr className="flex-grow border-t-1 border-gray-300" />
             </div>
+
             <ButtonGoogle text="Criar conta com o Google" />
           </form>
-        </div>
+        </FormProvider>
       </section>
     </main>
   );

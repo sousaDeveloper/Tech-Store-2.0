cbfb6e13e20c5ffec31d0c2ecd7b13ff973c8dbf
sousaDeveloper <devlpsousa@gmail.com>
2025-01-25 11:17:49 -0300

    style: set type password

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,22 +1,45 @@
 import * as React from "react";
-
 import { cn } from "@/lib/utils";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
   ({ className, type, ...props }, ref) => {
+    const [inputType, setInputType] = React.useState(type);
+
+    const handleInputType = () => {
+      setInputType((prevType) =>
+        prevType === "password" ? "text" : "password"
+      );
+    };
+
     return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-          className
+      <div className="relative">
+        <input
+          type={inputType}
+          className={cn(
+            "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+        {type === "password" && (
+          <span
+            onClick={handleInputType}
+            className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+          >
+            {inputType === "password" ? (
+              <EyeOffIcon size={20} />
+            ) : (
+              <EyeIcon size={20} />
+            )}
+          </span>
         )}
-        ref={ref}
-        {...props}
-      />
+      </div>
     );
   }
 );
+
 Input.displayName = "Input";
 
 export { Input };

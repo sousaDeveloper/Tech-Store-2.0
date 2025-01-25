724f1f92704e9a873e7a49990b4e34f59e8cf9b4
sousaDeveloper <devlpsousa@gmail.com>
2025-01-25 11:17:37 -0300

    feat: add error if email not exists

--- src/app/user/sign-in/page.tsx
+++ src/app/user/sign-in/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft, Loader2Icon } from "lucide-react";
 import Link from "next/link";
 import { FormProvider, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -15,7 +15,10 @@ import {
 import { Input } from "@/components/ui/input";
 import ButtonGoogle from "@/app/user/components/ButtonGoogle";
 import { useRouter } from "next/navigation";
-import { signIn, useSession } from "next-auth/react";
+import { signIn } from "next-auth/react";
+import { useToast } from "@/hooks/use-toast";
+import { useContext, useState } from "react";
+import { LoadingContext } from "@/providers/loading";
 
 const formSchema = z.object({
   email: z.string().email("Email inválido").min(1, "Email é obrigatório"),
@@ -26,7 +29,10 @@ const formSchema = z.object({
 });
 
 const SignInPage = () => {
+  const [authError, setAuthError] = useState<string | null>(null);
   const router = useRouter();
+  const { toast } = useToast();
+  const { isLoading, handleLoadingClick } = useContext(LoadingContext);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -37,6 +43,9 @@ const SignInPage = () => {
   });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    setAuthError(null);
+    handleLoadingClick(true);
+
     const signInData = await signIn("credentials", {
       email: values.email,
       password: values.password,
@@ -44,19 +53,21 @@ const SignInPage = () => {
     });
 
     if (signInData?.error) {
-      console.log(signInData.error);
+      setAuthError("Email ou senha incorretos.");
+      handleLoadingClick(false);
     } else {
-      router.push("/");
+      router.push("/user-profile");
+      toast({
+        title: "Login realizado com sucesso!",
+        description: "Redirecionando para página do usuário em instantes.",
+      });
+      handleLoadingClick(false);
     }
   };
 
-  const { data } = useSession();
-
-  console.log(data?.user?.name);
-
   return (
-    <section className="flex flex-col p-10 pt-5 text-secondaryColor min-h-screen text-center">
-      <div className="bg-blur-purple absolute top-0 left-0 w-full h-full z-[-1]"></div>
+    <section className="flex flex-col p-10 pt-5 text-secondaryColor min-h-[38rem] text-center">
+      <div className="bg-blur-purple absolute top-0 left-0 w-full min-h-[38rem] z-[-1]"></div>
       <div className="flex justify-between">
         <ChevronLeft
           size={36}
@@ -69,7 +80,7 @@ const SignInPage = () => {
       <FormProvider {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-5 mt-10"
+          className="space-y-4 mt-10"
         >
           <FormField
             control={form.control}
@@ -78,7 +89,14 @@ const SignInPage = () => {
               <FormItem className="text-left">
                 <FormLabel className="text-md">Email</FormLabel>
                 <FormControl>
-                  <Input type="text" {...field} />
+                  <Input
+                    type="text"
+                    {...field}
+                    onChange={(e) => {
+                      field.onChange(e);
+                      setAuthError(null);
+                    }}
+                  />
                 </FormControl>
                 <FormMessage className="text-red-400 text-start" />
               </FormItem>
@@ -92,29 +110,41 @@ const SignInPage = () => {
               <FormItem className="text-left">
                 <FormLabel className="text-md">Senha</FormLabel>
                 <FormControl>
-                  <Input type="password" {...field} />
+                  <Input
+                    type="password"
+                    {...field}
+                    onChange={(e) => {
+                      field.onChange(e);
+                      setAuthError(null);
+                    }}
+                  />
                 </FormControl>
                 <FormMessage className="text-red-400 text-start" />
               </FormItem>
             )}
           />
 
-          <button className="w-full py-2 bg-gradient mt-5 rounded-lg">
-            Acessar
+          {authError && (
+            <div className="text-red-400 text-start">{authError}</div>
+          )}
+
+          <button className="w-full py-2 bg-gradient mt-5 rounded-lg flex items-center justify-center gap-2">
+            Acessar{" "}
+            {isLoading && <Loader2Icon className="animate-spin" size={22} />}
           </button>
           <div className="flex justify-center items-center space-x-4 my-5">
             <hr className="flex-grow border-t-1 border-gray-300" />
             <h3 className="text-sm opacity-70">Ou faça login com</h3>
             <hr className="flex-grow border-t-1 border-gray-300" />
           </div>
-          <ButtonGoogle text="Continuar com o Google" />
-          <h3 className="text-sm text-center mt-4">
-            Não possui uma conta?{" "}
-            <Link className="underline" href="/user/sign-up">
-              Registre-se
-            </Link>
-          </h3>
         </form>
+        <ButtonGoogle text="Continuar com o Google" />
+        <h3 className="text-sm text-center mt-4">
+          Não possui uma conta?{" "}
+          <Link className="underline" href="/user/sign-up">
+            Registre-se
+          </Link>
+        </h3>
       </FormProvider>
     </section>
   );

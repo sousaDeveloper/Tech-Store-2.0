863db135aebe52a36b4bf72b6063b77bfaa11a47
sousaDeveloper <devlpsousa@gmail.com>
2025-01-27 20:28:16 -0300

    feat: add FavoriteButton and Header Component

---
+++ src/app/components/Product/FavoriteButton.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { ProductWithTotalPrice } from "@/helpers/product";
+import { HeartIcon } from "lucide-react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+
+interface FavoriteButtonProps {
+  product: ProductWithTotalPrice;
+}
+
+const FavoriteButton = ({ product }: FavoriteButtonProps) => {
+  const [favoriteItem, setFavoriteItem] = useState(product.favorite);
+  const [isLoading, setIsLoading] = useState(false);
+  const { status, data: session } = useSession();
+  const router = useRouter();
+
+  const handleAddFavoriteClick = async () => {
+    if (status === "authenticated" && !isLoading) {
+      setIsLoading(true);
+      toast(
+        `${
+          !favoriteItem
+            ? "Produto adicionado à Lista de Desejos!"
+            : "Produto removido da Lista de Desejos!"
+        }`,
+        {
+          action: {
+            label: "Ver Lista",
+            onClick: () => router.push("/user-profile/wishlist"),
+          },
+        }
+      );
+
+      const newFavoriteStatus = !favoriteItem;
+      setFavoriteItem(newFavoriteStatus);
+
+      try {
+        await fetch(`/api/product/${product.id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ favorite: newFavoriteStatus }),
+          credentials: "include",
+        });
+      } catch (error) {
+        console.error("Erro ao atualizar o favorito:", error);
+        setFavoriteItem((prev) => !prev);
+        toast.error("Erro ao atualizar o favorito.");
+      } finally {
+        setIsLoading(false);
+      }
+    } else if (status !== "authenticated") {
+      router.push("/api/auth/signin");
+    }
+  };
+
+  useEffect(() => {
+    if (session?.user?.favorites) {
+      setFavoriteItem(session.user.favorites.includes(product.id));
+    }
+  }, [session, product.id]);
+
+  return (
+    <button
+      className="z-10 rounded-lg p-1"
+      onClick={handleAddFavoriteClick}
+      disabled={isLoading}
+    >
+      <HeartIcon
+        size={30}
+        className={`ml-auto ${
+          favoriteItem ? "bg-primaryColor" : "bg-background"
+        } rounded-lg p-1 text-gray-400`}
+      />
+    </button>
+  );
+};
+
+export default FavoriteButton;
---
+++ src/app/user-profile/components/Header.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Separator from "@/app/components/Separator";
+import { LoadingContext } from "@/providers/loading";
+import { ChevronLeft, Loader2Icon } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+import { useContext } from "react";
+
+interface HeaderProps {
+  text: string;
+  className: string;
+}
+
+const Header = ({ text, className }: HeaderProps) => {
+  const router = useRouter();
+  const { handleLoadingClick, isLoading } = useContext(LoadingContext);
+  const pathname = usePathname();
+
+  const handleRouterBackClick = () => {
+    handleLoadingClick(true);
+    router.back();
+  };
+
+  return (
+    <>
+      <div className="flex items-center text-center mb-2">
+        <button onClick={handleRouterBackClick}>
+          <ChevronLeft size={40} className="cursor-pointer" />
+        </button>
+        <h1 className={`text-2xl ${className}`} data-aos="fade-down">
+          {text}
+        </h1>
+        {isLoading && (
+          <Loader2Icon
+            className={`animate-spin ml-16 ${
+              pathname === "/user-profile/wishlist" && "flex-none hidden"
+            }`}
+          />
+        )}
+      </div>
+      <Separator />
+    </>
+  );
+};
+
+export default Header;

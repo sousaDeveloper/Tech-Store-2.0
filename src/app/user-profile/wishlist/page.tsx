ec3481a2d233ede1a79c7c5f3b28b417c903074e
sousaDeveloper <devlpsousa@gmail.com>
2025-01-27 20:28:36 -0300

    feat: craete page of wish list and filter products

---
+++ src/app/user-profile/wishlist/page.tsx
@@ -0,0 +1,46 @@
+import { authOptions } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import Header from "../components/Header";
+import { computeProductTotalPrice } from "@/helpers/product";
+import ProductItem from "@/app/components/Product/ProductItem";
+import { redirect } from "next/navigation";
+
+const WishListPage = async () => {
+  const session = await getServerSession(authOptions);
+
+  const userFavorites = await prisma.favorite.findMany({
+    where: { userId: session?.user.id },
+    include: { product: true },
+  });
+
+  if (!session?.user) {
+    return redirect("/api/auth/signin");
+  }
+
+  return (
+    <main className="p-5 text-secondaryColor flex flex-col justify-center">
+      <Header text="Lista de Desejos" className="ml-12" />
+      {userFavorites.length === 0 ? (
+        <h1 className="mt-2">Nenhum produto em sua lista de desejos.</h1>
+      ) : (
+        <div className="flex flex-wrap gap-5 mt-5">
+          {userFavorites.map((p) => (
+            <ProductItem
+              key={p.product.id}
+              product={{
+                ...p.product,
+                totalPrice: computeProductTotalPrice(p.product),
+                favorite: true,
+              }}
+              dataAosDelay={0}
+              className="min-w-[10rem] max-w-[10rem]"
+            />
+          ))}
+        </div>
+      )}
+    </main>
+  );
+};
+
+export default WishListPage;

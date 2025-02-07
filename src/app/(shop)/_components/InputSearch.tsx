43baa438f85e4b71003609b777173af579f00759
sousaDeveloper <devlpsousa@gmail.com>
2025-02-07 16:29:53 -0300

    feat: add search loading state and submit on Enter key press

--- src/app/(shop)/_components/InputSearch.tsx
+++ src/app/(shop)/_components/InputSearch.tsx
@@ -2,8 +2,8 @@
 
 import { Category, Product } from "@prisma/client";
 import { SearchIcon } from "lucide-react";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useRouter, usePathname } from "next/navigation";
+import { useState, useEffect } from "react";
 
 const InputSearch = () => {
   const [query, setQuery] = useState("");
@@ -11,19 +11,44 @@ const InputSearch = () => {
     categories: Category[];
     products: Product[];
   }>({ categories: [], products: [] });
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
+  const pathname = usePathname();
 
-  const handleSearch = async () => {
-    if (query.trim()) {
-      router.push(`/product-find/${query}`);
-    } else {
-      return;
+  useEffect(() => {
+    if (pathname && pathname.includes("/product-find/")) {
+      const searchTerm = pathname.split("/product-find/")[1];
+      setQuery(searchTerm);
+      fetchResults(searchTerm);
     }
+  }, [pathname]);
 
+  const fetchResults = async (query: string) => {
+    setIsLoading(true);
     const res = await fetch(`/api/search?q=${query}`);
     const data = await res.json();
-
     setResults(data);
+    setIsLoading(false);
+  };
+
+  const handleSearch = async () => {
+    if (query.trim() && !isLoading) {
+      const searchTerm = `/product-find/${query}`;
+
+      if (pathname !== searchTerm) {
+        router.push(searchTerm);
+      } else {
+        return;
+      }
+
+      fetchResults(query);
+    }
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
   };
 
   return (
@@ -33,12 +58,13 @@ const InputSearch = () => {
         name="search"
         id="search"
         value={query}
-        placeholder="O que você procura?"
+        placeholder="Pesquise sua marca favorita"
         autoComplete="off"
         className="rounded-md flex justify-end px-3 py-2 w-[17rem] lg:w-[22rem] md:ml-10 lg:ml-20 bg-background"
         data-aos="fade-down"
         data-aos-delay="300"
         onChange={(e) => setQuery(e.target.value)}
+        onKeyDown={handleKeyPress}
       />
       <span
         className="absolute right-3 top-[0.5rem] transform -translate-y-1/2 cursor-pointer"

f0528b6f4ad0ca34e1f160079f35734b798fa836
sousaDeveloper <devlpsousa@gmail.com>
2025-02-11 14:40:31 -0300

    fix: change host_url

--- src/actions/checkout.ts
+++ src/actions/checkout.ts
@@ -14,7 +14,7 @@ export const createCheckout = async (
   const checkout = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     mode: "payment",
-    success_url: `${process.env.NEXT_PUBLIC_APP_URL}/checkout/success`,
+    success_url: `${process.env.HOST_URL}/checkout/success`,
     cancel_url: process.env.HOST_URL,
     metadata: {
       orderId,

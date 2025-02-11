161090db0d3421c4444c5fabcfff1f633339d384
sousaDeveloper <devlpsousa@gmail.com>
2025-02-11 14:32:11 -0300

    feat: add Accordion Component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,26 +2,49 @@ import type { Config } from "tailwindcss";
 import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "#1f1f1f",
-        primaryColor: "#4C30B5",
-        secondaryColor: "#E6F0FF",
-        backgroundItem: "#333739",
-      },
-      backgroundImage: {
-        "gradient-footer": "linear-gradient(to top, #000, #000, #1f1f1f)",
-      },
-      screens: {
-        "3xl": "1750px",
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: '#1f1f1f',
+  			primaryColor: '#4C30B5',
+  			secondaryColor: '#E6F0FF',
+  			backgroundItem: '#333739'
+  		},
+  		backgroundImage: {
+  			'gradient-footer': 'linear-gradient(to top, #000, #000, #1f1f1f)'
+  		},
+  		screens: {
+  			'3xl': '1750px'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		}
+  	}
   },
   plugins: [tailwindcssAnimate],
 } satisfies Config;
